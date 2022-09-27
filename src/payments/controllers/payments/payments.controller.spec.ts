0e16f43754ba8931de1991e8de0e043155963029
Samyak Shah <samyakshah3008@gmail.com>
2022-09-27 18:08:10 +0530

    unit testing with controllers done

--- src/payments/controllers/payments/payments.controller.spec.ts
+++ src/payments/controllers/payments/payments.controller.spec.ts
@@ -1,8 +1,22 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { Request, response, Response } from 'express';
 import { PaymentsController } from './payments.controller';
 
 describe('PaymentsController', () => {
   let controller: PaymentsController;
+
+  let requestMock = {
+    query: {}, 
+  } as unknown as Request;
+
+  let responseMock = {
+   status: jest.fn((x)=> ({
+    send: jest.fn((y)=> y)
+   }) ),
+   send: jest.fn((x)=> x)
+    
+  } as unknown as Response
+
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -10,9 +24,22 @@ describe('PaymentsController', () => {
     }).compile();
 
     controller = module.get<PaymentsController>(PaymentsController);
+  
   });
+
+
  
   it('should be defined', () => {
     expect(controller).toBeDefined();
-  });
+  }); 
+
+  describe('getPayments', ()=> {
+    it('should return status of 400', ()=> {
+      controller.getPayments(requestMock, responseMock)
+     expect(responseMock.status).toHaveBeenCalledWith(400)
+    })
+    
+  })
 });
+
+ 
--- src/payments/controllers/payments/payments.controller.ts
+++ src/payments/controllers/payments/payments.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Req, Res } from '@nestjs/common';
+import { Request, Response } from 'express';
 
 @Controller('payments')
-export class PaymentsController {}
+export class PaymentsController {
+    
+    @Get()
+    getPayments(@Req() req: Request, @Res() res: Response){
+        const {count,page} = req.query
+        if(!count || !page){
+            res.status(400).send({msg: "Missing any parameter"})
+        } else{
+            res.status(200).send({msg: "Success"})
+        }
+
+    }
+}
+
+// create a function with traditional expressJS format, destructure count and page from request. Check condition that if count or page is false, throw error 404 or send 200
